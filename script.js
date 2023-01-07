05b0240daaa909b8f2592398c23caa5268561899
Vignesh <76857655+VigneshKalyan@users.noreply.github.com>
2023-01-07 19:45:59 +0530

    Update script.js
    all changes done

--- script.js
+++ script.js
@@ -7,21 +7,59 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+let s=arr.map(function(a){
+     return `${a.id} ${a.name} ${a.age} ${a.profession=="developer"}`;
+ });
+return s;
 }
+
+console.log(PrintDeveloperswithMap());
+
+
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+    
+    let s=arr.forEach(function(value,key,a){
+        if(a[key].profession==="developer") 
+       console.log(`${a[key].id} ${a[key].name} ${a[key].age}`);
+     
+    });
+  
+
 }
+
+
+
+
 
 function addData() {
-  //Write your code here
+ let employee = {
+  id: 1, 
+  name: "john", 
+  age: "18",
+  profession: "developer" 
+  
+     };
+let h=[{id:4,name:"susan",age:"20",profession:"intern"}];
+
+h.push(employee);
+return h;
 }
+
+console.log(addData());
 
 function removeAdmin() {
-  //Write your code here
+  arr.splice(2);
 }
+removeAdmin();
+console.log(arr);
 
 function concatenateArray() {
-  //Write your code here
+    let arr1 = [
+  { id: 4, name: "john", age: "18", profession: "developer" },
+  { id: 5, name: "jack", age: "20", profession: "developer" },
+  { id: 6, name: "karen", age: "19", profession: "admin" },
+];
+  return arr.concat(arr1);
 }
+console.log(concatenateArray());
